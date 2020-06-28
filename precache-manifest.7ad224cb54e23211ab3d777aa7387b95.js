d86ea7dbfe1519ee0fd19c76d82a510320fcb948
Fernando Torres <fertorresmx@gmail.com>
2020-06-27 21:22:45 -0500

    Updates

---
+++ precache-manifest.7ad224cb54e23211ab3d777aa7387b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36808c568003a74ca4e9c5d46208a732",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "fe863201fbbaf3ab940f",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe863201fbbaf3ab940f",
+    "url": "/react-tutorial/static/js/main.f18cd4fa.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
